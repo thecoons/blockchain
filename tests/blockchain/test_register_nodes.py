5d8f3e95c4b2adbd675e7322cfb73b34387dc3fb
Barthelemey Antonin <antobarth@gmail.com>
2019-11-23 00:06:19 +0100

    Uncomment wrong tests pending

--- tests/blockchain/test_register_nodes.py
+++ tests/blockchain/test_register_nodes.py
@@ -1,25 +1,25 @@
-# from .test_case.blockchain import BlockchainTestCase
-# from model import Blockchain
+from .test_case.blockchain import BlockchainTestCase
+from model import Blockchain
 
 
-# class TestRegisterNodes(BlockchainTestCase):
-#     def test_valid_nodes(self):
-#         blockchain = Blockchain()
+class TestRegisterNodes(BlockchainTestCase):
+    def test_valid_nodes(self):
+        blockchain = Blockchain()
 
-#         blockchain.register_node("http://192.168.0.1:5000")
+        blockchain.register_node("http://192.168.0.1:5000")
 
-#         self.assertIn("192.168.0.1:5000", blockchain.nodes)
+        self.assertIn("192.168.0.1:5000", blockchain.nodes)
 
-#     def test_malformed_nodes(self):
-#         blockchain = Blockchain()
+    def test_malformed_nodes(self):
+        blockchain = Blockchain()
 
-#         with self.assertRaises(ValueError):
-#             blockchain.register_node("http//192.168.0.1:5000")
+        with self.assertRaises(ValueError):
+            blockchain.register_node("http//192.168.0.1:5000")
 
-#     def test_idempotency(self):
-#         blockchain = Blockchain()
+    def test_idempotency(self):
+        blockchain = Blockchain()
 
-#         blockchain.register_node("http://192.168.0.1:5000")
-#         blockchain.register_node("http://192.168.0.1:5000")
+        blockchain.register_node("http://192.168.0.1:5000")
+        blockchain.register_node("http://192.168.0.1:5000")
 
-#         assert len(blockchain.nodes) == 1
+        assert len(blockchain.nodes) == 1
