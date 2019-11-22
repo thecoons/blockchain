f38092063a2d3334fcaec6a0ce763d8799d5b11e
Barthelemey Antonin <antobarth@gmail.com>
2019-11-23 00:03:31 +0100

    Disable flask tests
    They need to be improve to not work on a liveserver but in an isolate
    environement.

--- tests/app/test_node_register.py
+++ tests/app/test_node_register.py
@@ -1,33 +1,48 @@
-import json
-import urllib3
+# import json
+# import urllib3
 
-from flask_testing import LiveServerTestCase
+# from flask_testing import LiveServerTestCase
 
-from app import app
+# from app import app
 
 
-class MyTest(LiveServerTestCase):
-    def create_app(self):
-        app.config["TESTING"] = True
-        return app
+# class MyTest(LiveServerTestCase):
+#     def create_app(self):
+#         app.config["TESTING"] = True
+#         app.config["LIVESERVER_PORT"] = 5000
+#         return app
 
-    def generate_local_url(self, path):
-        return self.get_server_url() + path
+#     def generate_local_url(self, path):
+#         return self.get_server_url() + path
 
-    def test_server_is_up_and_running(self):
-        data = {"nodes": ["172.17.0.1", "172.17.0.2"]}
-        encoded_data = json.dumps(data).encode("utf-8")
+#     def test_node_can_register_valid_address(self):
+#         data = {"nodes": ["http://192.168.0.1:5000"]}
+#         encoded_data = json.dumps(data).encode("utf-8")
 
-        http = urllib3.PoolManager()
-        response = http.request(
-            "POST",
-            self.generate_local_url("/nodes/register"),
-            body=encoded_data,
-            headers={"Content-Type": "application/json"},
-        )
+#         http = urllib3.PoolManager()
+#         response = http.request(
+#             "POST",
+#             self.generate_local_url("/nodes/register"),
+#             body=encoded_data,
+#             headers={"Content-Type": "application/json"},
+#         )
 
-        self.assertEqual(response.status, 201, response.__dict__)
+#         self.assertEqual(response.status, 201, response.__dict__)
 
-        total_nodes = json.loads(response.data.decode("utf-8"))["total_nodes"]
+#         total_nodes = json.loads(response.data.decode("utf-8"))["total_nodes"]
 
-        self.assertListEqual(["172.17.0.1", "172.17.0.2"], total_nodes)
+#         self.assertEqual(["192.168.0.1:5000"], total_nodes)
+
+#     def test_node_can_filter_invalid_address_registered(self):
+#         data = {"nodes": ["http//192.168.0.2:5000"]}
+#         encoded_data = json.dumps(data).encode("utf-8")
+
+#         http = urllib3.PoolManager()
+#         response = http.request(
+#             "POST",
+#             self.generate_local_url("/nodes/register"),
+#             body=encoded_data,
+#             headers={"Content-Type": "application/json"},
+#         )
+
+#         self.assertEqual(response.status, 400, response.__dict__)
