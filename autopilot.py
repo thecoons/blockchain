523159e42ab9cc8270faf810621945d663a85796
Barthelemey Antonin <antobarth@gmail.com>
2019-11-23 14:36:40 +0100

    add logs to autopilot

--- autopilot.py
+++ autopilot.py
@@ -1,8 +1,19 @@
+import logging
+import datetime
+
 from datetime import timedelta
 
 from timeloop import Timeloop
 
 from model.agent.factory import AgentFactory
+
+logging.basicConfig(
+    filename="logs/{}.log".format(
+        datetime.datetime.now().strftime("%b-%d-%I%M%p-%G")
+    ),
+    level=logging.INFO,
+    format="%(asctime)s %(message)s",
+)
 
 timeloop = Timeloop()
 agent = AgentFactory.create_from_base_configuration()
@@ -11,7 +22,7 @@ agent = AgentFactory.create_from_base_configuration()
 @timeloop.job(interval=timedelta(seconds=2))
 def clock():
     agent.run()
-    print("> agent.state = {}".format(agent.state))
+    logging.info("> agent.state = {}".format(agent.state))
 
 
 if __name__ == "__main__":
