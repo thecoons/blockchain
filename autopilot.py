0047436f25158e238b3bfb02d50a59512b290223
Barthelemey Antonin <antobarth@gmail.com>
2019-11-24 12:25:14 +0100

    Configuration autopilot logger

--- autopilot.py
+++ autopilot.py
@@ -7,13 +7,24 @@ from timeloop import Timeloop
 
 from model.agent.factory import AgentFactory
 
+format_template = "[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s"
+
 logging.basicConfig(
     filename="logs/{}.log".format(
         datetime.datetime.now().strftime("%b-%d-%I%M%p-%G")
     ),
     level=logging.INFO,
-    format="%(asctime)s %(message)s",
+    format=format_template,
 )
+
+formatter = logging.Formatter(format_template)
+console = logging.StreamHandler()
+console.setFormatter(formatter)
+console.setLevel(logging.INFO)
+
+logger = logging.getLogger("autopilot")
+logger.addHandler(console)
+
 
 timeloop = Timeloop()
 agent = AgentFactory.create_from_base_configuration()
@@ -22,7 +33,7 @@ agent = AgentFactory.create_from_base_configuration()
 @timeloop.job(interval=timedelta(seconds=2))
 def clock():
     agent.run()
-    logging.info("> agent.state = {}".format(agent.state))
+    logger.info("agent.state = {}".format(agent.state))
 
 
 if __name__ == "__main__":
